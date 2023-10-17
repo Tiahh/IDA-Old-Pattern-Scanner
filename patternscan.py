f4f5c42097fbb69165a3e21273190b38421b44f9
Tiahh <33553223+Tiahh@users.noreply.github.com>
2023-10-17 20:44:44 +0200

    Create patternscan.py

---
+++ patternscan.py
@@ -0,0 +1,219 @@
+patterns = {
+  "Ragdollv2": {
+    "signature": "00 BD 37 86 35 00 00 7A 45",
+    "declaration": "auto Ragdollv2_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "InfiniteJump": {
+    "signature": "CD CC CC 3D CD CC 8C 3F",
+    "declaration": "auto InfiniteJump_offset    =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "SpeedHack": {
+    "signature": "05 AA 58 4B 04 99 40 F8",
+    "declaration": "auto SpeedHack_offset =(getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "UnderGround1": {
+    "signature": "05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 04 00 00 00 0F 00 00",
+    "declaration": "auto UnderGround1_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "UnderGround2": {
+    "signature": "05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 04 00 00",
+    "declaration": "auto UnderGround2_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "UnderGround3": {
+    "signature": "05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 05 00 00",
+    "declaration": "auto UnderGround3_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "UnderGround4": {
+    "signature": "05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00",
+    "declaration": "auto UnderGround4_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NoWalkingAnimations": {
+    "signature": "E8 03 00 00 00 00 00 00 01 00 00 00 02 00 00 00",
+    "declaration": "auto NoWalkingAnimations_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Fly1": {
+    "signature": "05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 05 00",
+    "declaration": "auto Fly1_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Fly2": {
+    "signature": "05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 04 00 00 00 0F 00",
+    "declaration": "auto Fly2_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "AirWalk1": {
+    "signature": "05 00 00 00 12 00 00 00 08 00 00 00 08 00 00 00 12 00 00 00 08 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 08 00 00 00 04 00 00 00 12 00 00 00 0B 00 00 00",
+    "declaration": "auto AirWalk1_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "AirWalk2": {
+    "signature": "05 00 00 00 12 00 00 00 08 00 00 00 08 00 00 00 12 00 00 00 08 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 08 00 00 00 04 00 00 00 12 00 00 00 0C 00 00 00 0F 00 00 00 0F 00 00 00 03 00 00 00 12 00 00 00",
+    "declaration": "auto AirWalk2_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NoCli3": {
+    "signature": "05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 05",
+    "declaration": "auto NoCli3_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NoClip": {
+    "signature": "01 0A D7 23 3C 9D",
+    "declaration": "auto NoClip_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Camera_FreeZe": {
+    "signature": "9A 99 99 BF 9A 99 99 3F F0",
+    "declaration": "auto Camera_FreeZe_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Ragdollv1": {
+    "signature": "05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00",
+    "declaration": "auto Ragdollv1_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "DarkMode": {
+    "signature": "1F 85 AB 3F 42 60 E5 3C",
+    "declaration": "auto DarkMode_offset   =(getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NoBody": {
+    "signature": "00 00 00 40 00 00 80 3F 00 00 80 3F 00 00 00 40 00 00 80 3F 00 00 80",
+    "declaration": "auto NoBody_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Chams": {
+    "signature": "CD CC CC 3E 5C 8F C2 3E",
+    "declaration": "auto Chams_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "BlueAll": {
+    "signature": "00 50 C3 47 00 00 00 00",
+    "declaration": "auto BlueAll_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "AnotherWalking": {
+    "signature": "B5 80 96 18 CB 28 6B 6E 4E",
+    "declaration": "auto AnotherWalking_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Ragdollv3": {
+    "signature": "54 00 80 3F 58 FF 7F 3F 89 48 78 44",
+    "declaration": "auto Ragdollv3_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "MoreLights": {
+    "signature": "1F 85 AB 3F 42 60 E5 3C",
+    "declaration": "auto MoreLights_offset=   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Chamsv3": {
+    "signature": "1F 85 AB 3F 42 60 E5 3C",
+    "declaration": "auto Chamsv3_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "BunnyHoppy": {
+    "signature": "01 ?? ?? ?? 01 00 00 C8 42 CD CC CC 3D",
+    "declaration": "auto BunnyHoppy_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NoClipTheBest1": {
+    "signature": "00 BF 00 BF 00 BF 00 BF 00 BF 00 BF 00 BF 00 24 74 C9 00 24 74 C9 00 24 74 C9 00 24 74 C9 00 00 80 7F 00 00 80 7F 00 00 80 7F 00 00 80 7F 00 24 74 49 00 24 74 49 00 24 74 49 00 24 74 49 00 00 80 3F 00 00 80 3F",
+    "declaration": "auto NoClipTheBest1_offset    =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NoClipTheBest2": {
+    "signature": "00 BF 00 BF 00 BF 00 BF 00 BF 00 BF 00 BF 00 24 74 C9 00 24 74 C9 00 24 74 C9 00 24 74 C9 00 00 80 7F 00 00 80 7F 00 00 80 7F 00 00 80 7F 00 24 74 49 00 24 74 49 00 24 74 49 00 24 74 49 00 00 80 3F 00 00 80 3F",
+    "declaration": "auto NoClipTheBest2_offset    =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NoClipTheBest3": {
+    "signature": "C7 E7 00 24 74 C9 00 24 74 C9 00 24 74 C9 00 24 74 C9 00 00 80 7F 00 00 80 7F 00 00 80 7F 00 00 80 7F 00 24 74 49 00 24 74 49 00 24 74 49 00 24 74 49 00 00 80 3F 00 00 80 3F",
+    "declaration": "auto NoClipTheBest3_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "GodMode": {
+    "signature": "60 02 B0 BD EC 02 8B ?? ?? ?? ?? ?? ??",
+    "declaration": "auto GodMode_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "NightMode": {
+    "signature": "05 00 00 00 E1 0D 00 00 6F 80 00 00 13 85 00 00 1A 8C 00 00 03 00 00 00 01",
+    "declaration": "auto NightMode_offset   =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "ClimbMode": {
+    "signature": "08 00 00 00 0C 00 00 00 0C 00 00 00 0C 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00",
+    "declaration": "auto ClimbMode_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Fly3": {
+    "signature": "12 00 00 00 13 00 00 00 14 00 00 00 15 00 00 00 16 00 00 00 17 00 00 00 18 00 00 00 19 00 00 00 1A 00 00 00 1B 00 00 00 1C 00 00 00 1D 00 00 00 1E 00 00 00 1F 00 00 00 20 00 00 00 21 00 00 00 22 00 00 00 23 00 00 00 25 00 00 00 27 00 00 00 29 00 00 00 2B 00 00 00 2F 00 00 00 33 00 00 00 3B 00 00 00 43 00 00 00 53 00 00 00 63 00 00 00 83 00 00 00 03 01 00 00 03 02 00 00 03 04 00 00 03 08 00 00 03 10 00 00 03 20 00 00 03 40 00 00 03 80 00 00 03 00 01 00 00 00 00 00 01 00 00 00 01 00 00 00",
+    "declaration": "auto Fly3_offset    =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "SnakeMode1": {
+    "signature": "00 BD 37 86 35 00 00 7A 45",
+    "declaration": "auto SnakeMode1_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "SnakeMode2": {
+    "signature": "12 00 00 00 13 00 00 00 14 00 00 00 15 00 00 00 16 00 00 00 17 00 00 00 18 00 00 00 19 00 00 00 1A 00 00 00 1B 00 00 00 1C 00 00 00 1D 00 00 00 1E 00 00 00 1F 00 00 00 20 00 00 00 21 00 00 00 22 00 00 00 23 00 00 00 25 00 00 00 27 00 00 00 29 00 00 00 2B 00 00",
+    "declaration": "auto SnakeMode2_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "SnakeMode3": {
+    "signature": "12 00 00 00 03 00 00 00 0F 00 00 00 0F 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 05 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 04 00 00 00 0F 00 00 00 0F 00 00 00 03 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 01 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 02 00 00 00 05 00 00 00 0F 00 00 00 0F 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 0C 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 07 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 04 00 00 00 12 00 00 00 06 00 00 00 0F 00 00 00 0F 00 00 00",
+    "declaration": "auto SnakeMode3_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "TeleportUp": {
+    "signature": "01 ?? ?? ?? 01 00 00 C8 42 CD CC CC 3D",
+    "declaration": "auto TeleportUp_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "SlimBody1": {
+    "signature": "00 00 00 40 00 00 00 40 00 00 80 3F 00 00 80 3F 00 00 00 40",
+    "declaration": "auto SlimBody1_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "SlimBody2": {
+    "signature": "00 00 80 3F 00 00 00 40 00 00 80 3F 00 00 80 3F 00 00 00 40 00 00 80 3F 00 00 80",
+    "declaration": "auto SlimBody2_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "SlimBody3": {
+    "signature": "00 00 00 40 00 00 80 3F 00 00 80 3F 00 00 80 3F",
+    "declaration": "auto SlimBody3_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Teleport": {
+    "signature": "00 24 74 49 00 00 C8 42 00 50 C3 47",
+    "declaration": "auto Teleport_offset    =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "LightRespawn": {
+    "signature": "0A D7 23 3D 0A D7 23 3D 0A",
+    "declaration": "auto LightRespawn_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "ClimbMode2": {
+    "signature": "08 00 00 00 0C 00 00 00 0C 00 00 00 0C 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12",
+    "declaration": "auto ClimbMode2_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "LineSky": {
+    "signature": "05 00 00 00 E1 0D 00 00 6F 80 00 00",
+    "declaration": "auto LineSky_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Destroy1": {
+    "signature": "03 00 00 00 0F 00 00 00 0F 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00",
+    "declaration": "auto Destroy1_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Destroy2": {
+    "signature": "0F 00 00 00 0F 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 0C 00 00 00 12 00 00 00 12 00 00 00",
+    "declaration": "auto Destroy2_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Destroy3": {
+    "signature": "08 00 00 00 0F 00 00 00 0F 00 00 00 03 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 00 00 00 00 12 00 00 00 0C 00 00 00 12 00 00 00 12 00 00 00 05 00 00 00 12 00 00 00 12 00 00 00",
+    "declaration": "auto Destroy3_offset  =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Destroy4": {
+    "signature": "12 00 00 00 09 00 00 00 0F 00 00 00 0F 00 00 00 03 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 00 00 00 00 12 00 00 00 0C 00 00 00 12 00 00 00 12 00 00 00",
+    "declaration": "auto Destroy4_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Destroy5": {
+    "signature": "0F 00 00 00 0F 00 00 00 03 00 00 00 0B 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 0C 00 00 00 12 00 00 00 12 00 00 00 05 00 00 00",
+    "declaration": "auto Destroy5_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Destroy6": {
+    "signature": "0F 00 00 00 0F 00 00 00 03 00 00 00 12 00 00 00 12 00 00 00 0D 00 00 00 12 00 00 00 0E 00 00 00 12 00 00 00 00 00 00 00 12 00 00 00 12 00 00 00 08 00 00 00",
+    "declaration": "auto Destroy6_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Destroy7": {
+    "signature": "0F 00 00 00 0F 00 00 00 03 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 08 00 00 00 0E 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00 12 00 00 00",
+    "declaration": "auto Destroy7_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "LettersNamesSame": {
+    "signature": "CD CC 4C 3D 00 00 C8 42",
+    "declaration": "auto LettersNamesSame_offset    =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  },
+  "Jump2_offset": {
+    "signature": "E7 0A D7 23 3C BD 37 86 35 00 00 00 00",
+    "declaration": "auto Jump2_offset =   (getAbsoluteAddress(libName4, Obfuscator2Tiahh(OBFUSCATE(\"{OFFSET}\"))));"
+  }
+}
+
+print("-= START =-")
+for name, pattern in patterns.items():
+	ea = idc.find_binary(19, ida_search.SEARCH_DOWN, pattern["signature"])
+
+	if ea != ida_idaapi.BADADDR:
+	   print(pattern["declaration"].format(OFFSET=hex(ea)))
+
+print("-= END =-")
